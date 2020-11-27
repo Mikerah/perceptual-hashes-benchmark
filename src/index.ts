d037e96585928b7b712ae7e91bfb804f771f33bc
Sebastian Bolanos <sebasheston@protonmail.com>
2020-11-26 23:02:57 -0600

    Add some images

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,23 @@ import { pHash } from "./hashers";
 import fs from "fs";
 import { ethers } from "ethers";
 
-let image = fs.readFileSync("./static/fast.png");
-const main = async () => {
+const main = async (path: string) => {
+  let image = fs.readFileSync(path);
   pHash(image).then((bits: string) => {
     let entropy = ethers.utils.keccak256(Buffer.from(bits));
-    console.log(ethers.utils.HDNode.fromSeed(entropy).privateKey);
+    console.log(path, ethers.utils.HDNode.fromSeed(entropy).privateKey);
   })
 }
 
-main()
+let files = [
+  "fast-13.jpg",
+  "fast-compressed-237.jpg",
+  "fast-compressed-82.jpg",
+  "fast-compressed.jpg",
+  "fast.jpg",
+  "fast.png"
+];
+
+files.map((name) => {
+  main("./static/" + name)
+})

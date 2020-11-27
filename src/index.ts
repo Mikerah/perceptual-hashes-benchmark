0dbc90bc2ee8fa24947ec81dfeae139da9624862
Sebastian Bolanos <sebasheston@protonmail.com>
2020-11-27 11:12:13 -0600

    Removed the hashing of the entropy.
    BIP39 uses the entropy directly with out hashing.

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,20 @@ import { pHash } from "./hashers";
 import fs from "fs";
 import {format} from "util";
 import { ethers } from "ethers";
-const log = console.log
+const log = console.log;
 
 // https://keepass.info/help/kb/pw_quality_est.html
 // This has no randommness check and just checks length
-const bitLenghCheck = (bits: string): boolean => bits.length >= 128
+const bitLenghCheck = (bits: string): boolean => bits.length >= 32
 
 const generatePrivateKey = async (path: string): Promise<void> => {
   let image = fs.readFileSync(path);
-  let bits = await pHash(image, 15);
+  let bits = await pHash(image, 8);
   if (!bitLenghCheck(bits)) {
     throw "Not good enough entropy"
   }
-
-  let entropy = ethers.utils.sha512(ethers.utils.toUtf8Bytes(bits));
-  let pk =  ethers.utils.HDNode.fromSeed(entropy).privateKey;
-  log(format('image: %s\nsha256: %s\nperceptual hash: %s\nkey: %s\n', path, ethers.utils.sha256(image), bits, pk));
+  let wallet =  ethers.utils.HDNode.fromSeed(ethers.utils.toUtf8Bytes(bits));
+  log(format('image: %s\nsha256: %s\nperceptual hash: %s\nkey: %s\n', path, ethers.utils.sha256(image), bits, wallet.privateKey));
 }
 
 let mountains = [

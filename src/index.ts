bd1aab8cf10ad2a52dfd3b4666c22f626cbc7218
Sebastian Bolanos <sebasheston@protonmail.com>
2020-11-26 22:51:25 -0600

    Add private key of ethers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { pHash } from "./hashers";
-import fs from "fs"
+import fs from "fs";
+import { ethers } from "ethers";
 
 let image = fs.readFileSync("./static/fast.png");
-pHash(image).then(console.log)
+const main = async () => {
+  pHash(image).then((bits: string) => {
+    let entropy = ethers.utils.keccak256(Buffer.from(bits));
+    console.log(ethers.utils.HDNode.fromSeed(entropy).privateKey);
+  })
+}
+
+main()

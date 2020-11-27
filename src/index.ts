91ef21b9a7d2a8e1827842715b15b31fa6ac83ed
Sebastian Bolanos <sebasheston@protonmail.com>
2020-11-27 09:18:52 -0600

    Add generatePrivateKey

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,59 @@
 import { pHash } from "./hashers";
 import fs from "fs";
+import {format} from "util";
 import { ethers } from "ethers";
+const log = console.log
 
 // https://keepass.info/help/kb/pw_quality_est.html
 // This has no randommness check and just checks length
-const goodEntropyCheck = (bits: string): boolean => {
-  return bits.length >= 128
-}
+const bitLenghCheck = (bits: string): boolean => bits.length >= 128
 
-const main = async (path: string): Promise<void> => {
+const generatePrivateKey = async (path: string): Promise<void> => {
   let image = fs.readFileSync(path);
-  let bits = await pHash(image, 12)
-  console.log("Bits Length: ", bits.length);
-  if (!goodEntropyCheck(bits)) {
+  let bits = await pHash(image, 15);
+  if (!bitLenghCheck(bits)) {
     throw "Not good enough entropy"
   }
 
   let entropy = ethers.utils.sha512(ethers.utils.toUtf8Bytes(bits));
-  console.log(path, ethers.utils.HDNode.fromSeed(entropy).privateKey);
+  let pk =  ethers.utils.HDNode.fromSeed(entropy).privateKey;
+  log(format('image: %s\nsha256: %s\nperceptual hash: %s\nkey: %s\n', path, ethers.utils.sha256(image), bits, pk));
 }
 
-[
-  "fast-13.jpg",
-  "fast-compressed-237.jpg",
-  "fast-compressed-82.jpg",
-  "fast-compressed.jpg",
-  "fast.jpg",
-  "fast.png"
-].map((name) => {
-  main("./static/" + name).catch((e) => {
-    console.log(e)
-  })
-})
+let mountains = [
+  "mountain-telegram.jpg",
+  "mountain.jpg",
+  "mountain-gphotos.jpg",
+]
+let atila = [
+ "atila.jpg",
+ "atila-telegram.jpg",
+]
+
+let fast = [
+ "fast-13.jpg",
+ "fast-compressed-237.jpg",
+ "fast-compressed-82.jpg",
+ "fast-compressed.jpg",
+ "fast.jpg",
+ "fast.png",
+]
+
+const main = async() => {
+  for (let i in fast) {
+    await generatePrivateKey("./static/" + fast[i]).catch((e) => {
+      log(e)
+    })
+  }
+  for (let i in atila) {
+    await generatePrivateKey("./static/" + atila[i]).catch((e) => {
+      log(e)
+    })
+  }
+  for (let i in mountains) {
+    await generatePrivateKey("./static/" + mountains[i]).catch((e) => {
+      log(e)
+    })
+  }
+}
+main()
